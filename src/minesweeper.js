62e035e59df3b58a3bb476926c41aac930f09f31
cgonz0 <cgonzadesign@gmail.com>
2018-08-10 10:36:05 -0700

    commit project

---
+++ src/minesweeper.js
@@ -0,0 +1,53 @@
+// const blankline = '  |   |  ';
+// console.log("This is what an empty board would look like:");
+// console.log(blankline);
+// console.log(blankline);
+// console.log(blankline);
+
+// const guessline = '1 |   |  ';
+// const bombline = '  | B |  ';
+// console.log("This is what a board with a guess and a bomb on it would look like:");
+// console.log(guessline);
+// console.log(bombline);
+// console.log(blankline);
+
+// part 2
+// const exampleBoard = [
+//   [' ', ' ', ' '],
+//   [' ', ' ', ' '],
+//   [' ', ' ', ' ']
+// ];
+
+// const printBoard = board => {
+//   console.log("Current Board:");
+//   console.log(board[0].join(' | '));
+//   console.log(board[1].join(' | '));
+//   console.log(board[2].join(' | '));
+// }
+
+// console.log(printBoard(exampleBoard));
+
+// exampleBoard[0][1] = '1';
+// exampleBoard[2][2] = 'B';
+
+// console.log(printBoard(exampleBoard));
+
+
+
+
+
+// const g = new Game(3,3,3);
+// g.playMove(0,2);
+
+// const playerBoard = generatePlayerBoard(3,4);
+// const bombBoard = generateBombBoard(3,4,5);
+
+// console.log('Player Board: ');
+// printBoard(playerBoard);
+
+// console.log('Bomb Board: ');
+// printBoard(bombBoard);
+
+// flipTile(playerBoard, bombBoard, 2, 2);
+// console.log('Updated Player Board: ')
+// printBoard(playerBoard);
